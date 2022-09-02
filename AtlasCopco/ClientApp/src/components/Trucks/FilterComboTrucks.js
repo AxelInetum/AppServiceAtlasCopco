7430365b4f4047f107ac48ad734217981703bb37
SECTORIALES\X40369PI <axel.picon@inetum.com>
2022-09-02 13:11:40 +0200

    parte de buget finalizado

--- AtlasCopco/ClientApp/src/components/Trucks/FilterComboTrucks.js
+++ AtlasCopco/ClientApp/src/components/Trucks/FilterComboTrucks.js
@@ -104,8 +104,12 @@ const FilterComboTrucks= () => {
                     <div class="col-sx-12 col-sm-3 mb-4">
                         <Link to={'/CreateTrucks'} className="btn btn-primary bt-lg mb-2 fs-3">{t('CrearCamion')}</Link>    
                     </div>
-                    <div class="col-sx-12 col-sm-3 mb-4">
-                        <button class="btn btn-primary mr-1" onClick={() => OpenPopupFilter()}>hola</button> 
+                    <div class="col-sx-12 col-sm-6 mb-4"> 
+                    {FilterTruck.valueFilter != ""  ? (<a><span class="badge bg-primary">1</span></a> ) : ""}                                          
+                        <svg  onClick={() => OpenPopupFilter()}  xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+                                <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
+                        </svg>
+                        <p  class="fs-3 displayinlineblock">Filtros</p>
                     <div>
         
                 <Modal isOpen={showFilterTruckpopup} fade={false} style={modalStyle}>
